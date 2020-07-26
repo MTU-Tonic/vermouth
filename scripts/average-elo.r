a7dd21985e546ddbddc121968e6706aec9d21d49
Jason Hiebel <jshiebel@mtu.edu>
2020-07-26 12:11:15 -0400

    Added standard error for distribution of the means and beautified the presentation a little.

--- scripts/average-elo.r
+++ scripts/average-elo.r
@@ -10,15 +10,16 @@ args   <- parse_args(parser)
 rounds  <- read.csv(unz(args$run, "rounds.csv"))
 players <- read.csv(unz(args$run, "players.csv"))
 rounds$AVG_RANKING <- ave(rounds$RANKING, rounds$PLAYER, FUN=cumsum) / ave(rounds$RANKING, rounds$PLAYER, FUN=seq_along)
+rounds$STD_RANKING <- ave((rounds$RANKING - rounds$AVG_RANKING)^2, rounds$PLAYER, FUN=cumsum) / (ave(rounds$RANKING, rounds$PLAYER, FUN=seq_along) - 1)
+rounds$SDE_RANKING <- rounds$STD_RANKING / sqrt(ave(rounds$RANKING, rounds$PLAYER, FUN=seq_along))
 rounds$PLAYER <- players$NAME[factor(rounds$PLAYER)]
 
-rounds[1:12,]
-
 library(ggplot2)
-ggplot(rounds, aes(x=ROUND, y=AVG_RANKING, group=PLAYER, color=PLAYER)) +
+ggplot(rounds, aes(x=ROUND, y=AVG_RANKING, color=PLAYER, fill=PLAYER)) +
 	geom_line() +
-	scale_colour_discrete(name="Player") +
+	geom_ribbon(aes(ymin=AVG_RANKING-SDE_RANKING, ymax=AVG_RANKING+SDE_RANKING), linetype=0, alpha=0.3) +
 	xlab("Rounds") +
 	ylab("Average ELO Ranking") +
+	labs(color="Player", fill="Player") +
 	theme_bw()
 ggsave(paste(args$run, "avg-elo", "pdf", sep="."), device="pdf", width=9, height=4.5)
